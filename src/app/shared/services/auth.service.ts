10a962d6d8e6d254d91ea7255ce84696970ea45a
shiziks <shiziks@hotmail.com>
2023-06-24 12:10:00 +0200

    auth service changed url

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -9,57 +9,59 @@ import { IUser } from '../interfaces/i-user';
 export class AuthService {
 
   isAuth:any;
+  localhost:string="http://127.0.0.1:8000";
+  url:string="http://www.melog.000.pe";
   constructor(private http:HttpClient) { }
 
   //CHECK
   check():Observable<any>{
-    return this.http.get('http://127.0.0.1:8000/api/check');
+    return this.http.get(this.url+'/api/check');
   }
   
   //REGISTRATION
   register(user:IUser):Observable<any>{
-    return this.http.post('http://127.0.0.1:8000/api/register',user);
+    return this.http.post(this.url+'/api/register',user);
   }
 
   //LOGIN
   login(user:IUser):Observable<any>{
-    return this.http.post('http://127.0.0.1:8000/api/login',user); 
+    return this.http.post(this.url+'/api/login',user); 
   }
 
   //USER PROFILE
   profile(token:any):Observable<any>{
-    return this.http.post('http://127.0.0.1:8000/api/profile',token); 
+    return this.http.post(this.url+'/api/profile',token); 
   }
 
   //LOGOUT
   logout():Observable<any>{
-    return this.http.post('http://127.0.0.1:8000/api/logout',{});
+    return this.http.post(this.url+'/api/logout',{});
   }
 
 
   ///REFRESH
   refresh(token?:any):Observable<any>{
-    return this.http.post('http://127.0.0.1:8000/api/refresh', token);
+    return this.http.post(this.url+'/api/refresh', token);
   }
 
   ///PASSWORD RESET EMAIL
   passwordResetEmail(email:Object):Observable<any>{
-    return this.http.post('http://127.0.0.1:8000/api/sendemail', email);
+    return this.http.post(this.url+'/api/sendemail', email);
   }
 
   ///RESET PASSWORD
   passwordReset(data:any):Observable<any>{
-    return this.http.post('http://127.0.0.1:8000/api/resetpassword', data);
+    return this.http.post(this.url+'/api/resetpassword', data);
   }
 
   ////CHANGE PASSWORD
   changePassword(data:any):Observable<any>{
-    return this.http.post('http://127.0.0.1:8000/api/changepassword', data);
+    return this.http.post(this.url+'/api/changepassword', data);
   }
 
   /////RESEND VERIFY EMAIL
   resendemail(data:any):Observable<any>{
-    return this.http.get('http://127.0.0.1:8000/api/email/resend/'+data);
+    return this.http.get(this.url+'/api/email/resend/'+data);
   }
   
 }
